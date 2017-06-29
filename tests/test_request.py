476ae2989b621b35a8c5d78c8a0b5c348275c170
Jordan Jambazov <jordan.jambazov@codeideo.com>
2017-06-29 19:07:17 +0300

    Add validation test

--- tests/test_request.py
+++ tests/test_request.py
@@ -1,4 +1,5 @@
 import datetime
+import pytest
 from borica import Request
 
 
@@ -26,3 +27,18 @@ def test_general_request_base64_formatting():
         "R0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dH"
         "R0dH")
     assert expected_request == str(request)
+
+
+def test_protocol_version_validation():
+    with pytest.raises(ValueError) as excinfo:
+        Request(
+            transaction_type=10,
+            transaction_amount='99.99',
+            transaction_timestamp=datetime.datetime.fromtimestamp(0),
+            terminal_id='12345678',
+            order_id='12345678',
+            order_summary='Money for fun!',
+            signature=FakeSignature(),
+            protocol_version='0.0.0'
+        )
+        assert 'got 0.0.0' in excinfo.value
