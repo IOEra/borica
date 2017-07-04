06c52fce0af8be2dfccb268d55daaadf3bae3a4a
Jordan Jambazov <jordan.jambazov@codeideo.com>
2017-07-04 20:33:31 +0300

    Add fields to Borica response

--- borica/response.py
+++ borica/response.py
@@ -1,5 +1,6 @@
 import base64
 import binascii
+from datetime import datetime
 
 
 class Response:
@@ -23,4 +24,31 @@ class Response:
 
     @property
     def status_code(self):
-        return int(self.payload[51:53].decode())
+        return int(self._get_field(51, 53))
+
+    @property
+    def transaction_code(self):
+        return int(self._get_field(0, 2))
+
+    @property
+    def transaction_time(self):
+        return datetime.strptime(self._get_field(2, 16), '%Y%m%d%H%M%S')
+
+    @property
+    def amount(self):
+        return int(self._get_field(16, 28)) / 100.0
+
+    @property
+    def terminal_id(self):
+        return int(self._get_field(28, 36))
+
+    @property
+    def order_id(self):
+        return self._get_field(31, 51)
+
+    @property
+    def protocol_version(self):
+        return self._get_field(53, 56)
+
+    def _get_field(self, start, end):
+        return self.payload[start:end].decode()
--- tests/test_response.py
+++ tests/test_response.py
@@ -1,3 +1,4 @@
+import datetime
 from borica import Response, Verifier
 
 
@@ -31,7 +32,14 @@ def test_response():
     verifier = Verifier(CERTIFICATE)
     response = Response(RESPONSE)
     assert response.verify(verifier) is True
+    assert response.transaction_code == 10
+    assert response.transaction_time == datetime.datetime(
+        year=2017, month=6, day=30, hour=12, minute=35, second=40)
+    assert response.amount == 0.09
+    assert response.terminal_id == 62161072
+    assert response.order_id == "61072123451234512341"
     assert response.status_code == 94
+    assert response.protocol_version == "1.1"
 
 
 def test_bad_signature_response():
